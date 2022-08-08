7d259df3b727ab7718b7a004b867204ccef1b0bb
chin <a5566qq2581@gmail.com>
2022-08-08 14:27:14 +0800

    feat: default empty when plugin config not exists

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,21 @@ import { Application } from "express";
 import { exampleRouter } from "./api/example";
 import { userRouter } from "./api/user";
 import { aiServiceRouter } from "./api/ai-services";
-import { webRouter } from "./web/index";
-import { pluginsConfig } from "./plugins/config";
+import { webRouter } from "./web/index/index";
+
+async function importPluginConfig() {
+    try {
+        // @ts-ignore
+        let config = await import("./plugins/config"); 
+        return config;
+    } catch(e) {
+        console.log("can not find plugin config, use empty config");
+        return {};
+    }
+}
 
 export default async function(app: Application) {
+    let pluginsConfig = await importPluginConfig();
     for (let pluginName in pluginsConfig) {
         let plugin = (pluginsConfig as any)[pluginName];
         if (plugin.before && plugin.enable) {
